596822cea9fb4737624381750dc4ef12a76f5d56
Adam-Smart <adams8632@hotmail.co.uk>
2023-06-26 18:17:48 +0100

    added  fsutils and added get function for notes.js

---
+++ routes/notes.js
@@ -0,0 +1,26 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const {
+  readFromFile
+} = require('../helpers/fsUtils');
+
+// GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// GET Route for a specific tip
+notes.get('/:note_id', (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.id === noteId);
+      return result.length > 0
+        ? res.json(result)
+        : res.json('No notes with that ID');
+    });
+});
+
+
+module.exports = notes;
