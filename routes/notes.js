a1d829e92c79bd418cc39267f730d126e315e656
Adam-Smart <adams8632@hotmail.co.uk>
2023-06-26 18:29:08 +0100

    added delete and read to notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,9 @@
 const notes = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const {
-  readFromFile
+  readFromFile,
+  readAndAppend,
+  writeToFile,
 } = require('../helpers/fsUtils');
 
 // GET Route for retrieving all the notes
@@ -22,5 +24,41 @@ notes.get('/:note_id', (req, res) => {
     });
 });
 
+// DELETE Route for a specific tip
+notes.delete('/:note_id', (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all notes except the one with the ID provided in the URL
+      const result = json.filter((note) => note.id !== noteId);
+
+      // Save that array to the filesystem
+      writeToFile('./db/db.json', result);
+
+      // Respond to the DELETE request
+      res.json(`${noteId} was deleted`);
+    });
+});
+
+// POST Route for a new UX/UI tip
+notes.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { title, text} = req.body;
+
+  if (req.body) {
+    const newnote = {
+      title,
+      text,
+      note_id: uuidv4(),
+    };
+
+    readAndAppend(newnote, './db/db.json');
+    res.json(`note was added`);
+  } else {
+    res.error('Error adding note');
+  }
+});
 
 module.exports = notes;
